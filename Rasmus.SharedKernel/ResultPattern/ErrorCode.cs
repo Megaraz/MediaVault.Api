31a0fbd1a11178900808b0e8979c6517f6b985be
Megaraz <rasmus-lackberg@hotmail.com>
2026-03-18 11:56:58 +0100

    Removed ErrorCode factories, removed ErrorCodeType.cs, removed ResultFactoryHelpers

--- Rasmus.SharedKernel/ResultPattern/ErrorCode.cs
+++ Rasmus.SharedKernel/ResultPattern/ErrorCode.cs
@@ -32,9 +32,10 @@ namespace Rasmus.SharedKernel.ResultPattern
         GeneralUnauthorized = 303,
         GeneralForbidden = 304
     }
+
     // Represents a structured error code that encapsulates the operation, entity type, and error type.
     // The property Code is a concatenation of the three components, making it easy to identify and categorize errors in a consistent manner.
-    // Code will look like: "Create.User.ValidationError.Required" or "GetCollection.Order.DatabaseError.Timeout"
+    // Code will look like: "Create.User..Required" or "GetCollection.Order.Timeout"
     public sealed record ErrorCode
     {
         public OperationType Operation { get; }
@@ -51,46 +52,8 @@ namespace Rasmus.SharedKernel.ResultPattern
             Reason = reason;
         }
 
-        public static ErrorCode For<T>(OperationType operation, ErrorReasonCode reason) => 
+        public static ErrorCode For<T>(OperationType operation, ErrorReasonCode reason) =>
             new(operation, typeof(T).Name, reason);
-
-        // Factory methods for common error scenarios
-
-        //// Generate an error code for a Create-Operation on a specific entity type
-        //public static ErrorCode Create<T>(ErrorReasonCode reason) =>
-        //    new(OperationType.Create, typeof(T).Name, reason);
-
-        //// Generate an ErrorCode for a Get-Operation on a specific entity type
-        //public static ErrorCode Get<T>(ErrorReasonCode reason) =>
-        //    new(OperationType.Get, typeof(T).Name, reason);
-
-        //// Generate an ErrorCode for a GetCollection-Operation on a specific entity type
-        //public static ErrorCode GetCollection<T>(ErrorReasonCode reason) =>
-        //    new(OperationType.GetCollection, typeof(T).Name, reason);
-        //// Generate an ErrorCode for an Update-Operation on a specific entity type
-        //public static ErrorCode Update<T>(ErrorReasonCode reason) =>
-        //    new(OperationType.Update, typeof(T).Name, reason);
-        //// Generate an ErrorCode for a Delete-Operation on a specific entity type
-        //public static ErrorCode Delete<T>(ErrorReasonCode reason) =>
-        //    new(OperationType.Delete, typeof(T).Name, reason);
-
-        //// Generate an ErrorCode for a Required error on a specific entity type and operation
-        //public static ErrorCode Required<T>(OperationType currentOperation) =>
-        //    new(currentOperation, typeof(T).Name, ErrorReasonCode.ValidationRequired);
-
-        //public static ErrorCode InvalidFormat<T>(OperationType currentOperation) =>
-        //    new(currentOperation, typeof(T).Name, ErrorReasonCode.ValidationInvalidFormat);
-
-        //public static ErrorCode OutOfRange<T>(OperationType currentOperation) =>
-        //    new(currentOperation, typeof(T).Name, ErrorReasonCode.ValidationOutOfRange);
-        //public static ErrorCode TooShort<T>(OperationType currentOperation) =>
-        //    new(currentOperation, typeof(T).Name, ErrorReasonCode.ValidationTooShort);
-
-        //public static ErrorCode TooLong<T>(OperationType currentOperation) =>
-        //    new(currentOperation, typeof(T).Name, ErrorReasonCode.ValidationTooLong);
-
-        //public static ErrorCode Custom<T>(OperationType currentOperation) =>
-        //    new(currentOperation, typeof(T).Name, ErrorReasonCode.Custom);
 
     }
 }
