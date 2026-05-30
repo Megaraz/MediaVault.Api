924d2579ed9528a8250885d0741b32f85e4c7326
Megaraz <rasmus-lackberg@hotmail.com>
2026-05-30 15:35:47 +0200

    WIP

--- Rasmus.SharedKernel/Interfaces/ErrorLogger/IErrorLogger.cs
+++ Rasmus.SharedKernel/Interfaces/ErrorLogger/IErrorLogger.cs
@@ -5,7 +5,7 @@ namespace Rasmus.SharedKernel.Interfaces.ErrorLogger
     public interface IErrorLogger
     {
         Task CleanOldLogsAsync(CancellationToken ct = default);
-        Task<List<ErrorLog>> GetErrorLogsAsync(CancellationToken ct = default);
+        Task<IReadOnlyList<ErrorLog>> GetErrorLogsAsync(CancellationToken ct = default);
         Task LogErrorToFileAsync(Error error, CancellationToken ct = default);
     }
 }
